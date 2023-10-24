6b4573d06aec0e090cac67ed3651febf45a3f6e6
Coralis Burgos <csburgosrodriguez@student.fullsail.edu>
2023-10-24 13:40:35 -0400

    Testing in NodeJS

---
+++ string/string.js
@@ -0,0 +1,8 @@
+const uppercase = (str) => str.toUpperCase();
+
+//lowercase to lowercase function
+const lowercase = (str) => str.toLowerCase()
+module.exports = {
+    uppercase,
+    lowercase
+}
---
+++ string/string.test.js
@@ -0,0 +1,12 @@
+const { uppercase } = require("./string");
+const { lowercase } = require("./string")
+
+describe("Testing the string module", () => {
+    test("should uppercase a string input", () => {
+        expect(uppercase("bob")).toBe("BOB");
+    });
+
+    test("should lowercase a string input", () => {
+        expect(lowercase("FULL Sail")).toBe("full sail");
+    });
+});
